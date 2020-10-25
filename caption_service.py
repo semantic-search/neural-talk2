e6edbd553087c16fddf227ca34053698ae80a0c4
Akshay Ashok <aks28id@gmail.com>
2020-10-25 13:49:21 +0530

    updated to new kafka added logging

--- caption_service.py
+++ caption_service.py
@@ -1,16 +1,12 @@
 import json
-import subprocess
 import os
 from pathlib import Path
-
-
 
 result_dir = "vis/"
 result_file = result_dir + "vis.json"
 
-def predict(file_name, doc=False):
+def predict(file_name):
    
-
     os.system("th eval.lua -model model_id1-501-1448236541.t7 -image_folder images -num_images 1")
     with open(result_file) as json_file:
         data = json.load(json_file)
@@ -18,16 +14,7 @@ def predict(file_name, doc=False):
     for items in data:
         caption.append(items["caption"])
     
-    if doc:
-        response = {
-            "captions": caption,
-        }
-    else:
-        response = {
-            "file_name": file_name,
-            "captions": caption,
-            "is_doc_type": False
-        }
+    response = " ".join(caption)
 
     os.remove(file_name)
     os.remove(result_dir + "imgs/img1.jpg")
--- main.py
+++ main.py
@@ -1,77 +1,105 @@
-import json
 import uuid
 from db_models.mongo_setup import global_init
 from db_models.models.cache_model import Cache
 import init
+from init import ERR_LOGGER
 from caption_service import predict
 import globals
-import numpy
+import requests
 import os
-
-
-def send_to_topic(topic, value_to_send_dic):
-    # default=convert only used in this project
-    data_json = json.dumps(value_to_send_dic)
-    init.producer_obj.send(topic, value=data_json)
 
 imageFolder = "images"
 os.mkdir(imageFolder)
 
+global_init()
+
+FILE_ID = ""
+
+def save_to_db(db_object, result_to_save):
+    try:
+        print("*****************SAVING TO DB******************************")
+        if db_object.text:
+            db_object.text = db_object.text + ' ' + result_to_save
+        else:
+            db_object.text = result_to_save
+        db_object.save()
+        print("*****************SAVED TO DB******************************")
+    except Exception as e:
+        print(f"{e} ERROR IN SAVE TO DB FILE ID {FILE_ID}")
+        ERR_LOGGER(f"{e} ERROR IN SAVE TO DB FILE ID {FILE_ID}")
+
+def update_state(file):
+    payload = {
+        'topic_name': globals.RECEIVE_TOPIC,
+        'client_id': globals.CLIENT_ID,
+        'value': file
+    }
+    try:
+        requests.request("POST", globals.DASHBOARD_URL,  data=payload)
+    except Exception as e: 
+        print(f"{e} EXCEPTION IN UPDATE STATE API CALL......FILE ID {FILE_ID}")
+        ERR_LOGGER(f"{e} EXCEPTION IN UPDATE STATE API CALL......FILE ID {FILE_ID}")
+
 if __name__ == "__main__":
-    global_init()
-    print('main fxn', globals.RECEIVE_TOPIC)
+    print("Connected to Kafka at " + globals.KAFKA_HOSTNAME + ":" + globals.KAFKA_PORT)
+    print("Kafka Consumer topic for this Container is " + globals.RECEIVE_TOPIC)
+ 
     for message in init.consumer_obj:
         message = message.value
         db_key = str(message)
         print(db_key, 'db_key')
+        FILE_ID = db_key
         db_object = Cache.objects.get(pk=db_key)
         file_name = db_object.file_name
-        init.redis_obj.set(globals.RECEIVE_TOPIC, file_name)
-        print('after redis')
+
+        print("#############################################")
+        print("########## PROCESSING FILE " + file_name)
+        print("#############################################")
+ 
         if db_object.is_doc_type:
             """document"""
-            print('in doc type')
-            images_array = []
-            for image in db_object.files:
-                pdf_image = imageFolder + "/" + str(uuid.uuid4()) + ".jpg"
-                with open(pdf_image, 'wb') as file_to_save:
-                    file_to_save.write(image.file.read())
-                images_array.append(pdf_image)
-            captions_list = []
-            for image in images_array:
-                image_results = predict(image, doc=True)
-                captions = image_results["captions"]
-                captions_list.append(captions)
+            if db_object.contains_images:
+                images_array = []
+                for image in db_object.files:
+                    pdf_image = imageFolder + "/" + str(uuid.uuid4()) + ".jpg"
+                    with open(pdf_image, 'wb') as file_to_save:
+                        file_to_save.write(image.file.read())
+                    images_array.append(pdf_image)
 
-            full_res = {
-                "container_name": globals.RECEIVE_TOPIC,
-                "file_name": file_name,
-                "captions": captions_list,
-                "is_doc_type": True
-            }
-            text_res = {
-                "container_name": globals.RECEIVE_TOPIC,
-                "file_name": file_name,
-                "captions": captions_list,
-                "is_doc_type": True
-            }
-            print(full_res, "full_res")
-            send_to_topic(globals.SEND_TOPIC_FULL, value_to_send_dic=full_res)
-            send_to_topic(globals.SEND_TOPIC_TEXT, value_to_send_dic=text_res)
-            init.producer_obj.flush()
+                result_list = list()
+                for image in images_array:
+                    try:
+                        image_results = predict(image)
+                    except Exception as e:
+                        print(f"{e} Exception in predict")
+                        ERR_LOGGER(f"{e} Exception in predict FILE ID {FILE_ID}")
+                        continue
+
+                    result_list.append(image_results)
+                to_save = ' '.join(result_list)
+                print("to_save", to_save)
+                save_to_db(db_object, to_save)
+                print(".....................FINISHED PROCESSING FILE.....................")
+                update_state(file_name)
+            else:
+                pass
 
         else:
             """image"""
-            print('in image type')
-            if db_object.mime_type in globals.ALLOWED_IMAGE_TYPES:
-                image = imageFolder + "/" + file_name
+            
+            image = imageFolder + "/" + file_name
 
-                with open(image, 'wb') as file_to_save:
-                    file_to_save.write(db_object.file.read())
-                res = predict(image)
-                res["container_name"] = globals.RECEIVE_TOPIC
-                
-                print(res, 'res')
-                send_to_topic(globals.SEND_TOPIC_FULL, value_to_send_dic=res)
-                send_to_topic(globals.SEND_TOPIC_TEXT, value_to_send_dic=res)
-                init.producer_obj.flush()
+            with open(image, 'wb') as file_to_save:
+                file_to_save.write(db_object.file.read())
+            try:
+                image_results = predict(image)
+            except Exception as e:
+                print(f"{e} Exception in predict")
+                ERR_LOGGER(f"{e} Exception in predict FILE ID {FILE_ID}")
+                continue
+            to_save = image_results
+            print("to_save", to_save)
+            save_to_db(db_object, to_save)
+            print(".....................FINISHED PROCESSING FILE.....................")
+            update_state(file_name)
+    
